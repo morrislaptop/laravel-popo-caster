5a51859a1e46bd1b6642a6acc68160cf31ec897f
morrislaptop <morrislaptop@users.noreply.github.com>
2021-01-25 10:31:17 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Morrislaptop\Caster\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Morrislaptop\Caster\CasterServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
